16782a7720e588a7cd15b98b9a2461f98ab0cede
Ahnjaewon <vutus5@Naver.com>
2026-02-25 17:30:32 +0900

    chore: .gitignore 업데이트 및 불필요한 bin 폴더 파일 제거

--- backend/bin/main/db/migration/V3__user_security_profile_columns.sql
+++
@@ -1,6 +0,0 @@
-﻿-- V3__user_security_profile_columns.sql
--- address 컬럼 추가
-ALTER TABLE users ADD COLUMN address VARCHAR(255) NULL AFTER phone;
-
--- two_factor_enabled 컬럼 추가
-ALTER TABLE users ADD COLUMN two_factor_enabled TINYINT(1) NOT NULL DEFAULT 0 AFTER status;
